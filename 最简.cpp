efb60351d1fc2e6c2d938a4e420a96cef57f8543
离笙 <46399288+LiShengAlone@users.noreply.github.com>
2019-01-26 07:35:15 -0600

    Add files via upload

---
+++ 最简.cpp
@@ -0,0 +1,41 @@
+#include <windows.h>
+LRESULT CALLBACK WndProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam) {
+	switch (Message) {
+	case WM_DESTROY: {
+		PostQuitMessage(0);
+		break;
+	}
+	default:
+		return DefWindowProc(hwnd, Message, wParam, lParam);
+	}
+	return 0;
+}
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	WNDCLASSEX wc;
+	HWND hwnd;
+	MSG msg;
+	memset(&wc, 0, sizeof(wc));
+	wc.cbSize = sizeof(WNDCLASSEX);
+	wc.lpfnWndProc = WndProc;
+	wc.hInstance = hInstance;
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wc.lpszClassName = L"WindowClass";
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+	if (!RegisterClassEx(&wc)) {
+		MessageBox(NULL, L"Window Registration Failed!", L"Error!", MB_ICONEXCLAMATION | MB_OK);
+		return 0;
+	}
+	hwnd = CreateWindowEx(WS_EX_CLIENTEDGE, L"WindowClass", L"Caption", WS_VISIBLE | WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 640, 480, NULL, NULL, hInstance, NULL);
+	if (hwnd == NULL) {
+		MessageBox(NULL,L"Window Creation Failed!", L"Error!", MB_ICONEXCLAMATION | MB_OK);     //´°¿Ú´´ÔìÊ§°Ü 
+		return 0;
+	}
+	while (GetMessage(&msg, NULL, 0, 0) > 0) {
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return msg.wParam;
+}
